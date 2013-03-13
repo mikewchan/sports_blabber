ab48e02f093c9cf1d4a6fba36008966476ce1e88
mikewchan <mike@mikewchan.com>
2013-03-13 08:54:18 -0400

    Deleted initial AWS S3 stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,5 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :pins
 
-  # Uploading of image using AWS S3
-  has_attached_file :pin,
-  :styles =>{
-    :thumb  => "50x50",
-    :medium => "400x400"
-  },
-  :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml"",
-  :path => ":attachment/:id/:style.:extension",
-  :bucket => "OMRails2"
-
+  
 end
